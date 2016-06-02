71c05bcb49425bcbd5e51662dbd7e073da6fe15b
Sam Weiller <sam.weiller@gmail.com>
2016-06-02 09:27:01 -0700

    first commit
    first

---
+++ web/config.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var SPOTIFY_CLIENT_ID = '39b67cb936384107bdc3a037eec40faf';
+var SPOTIFY_REDIRECT_URI = 'http://localhost:8888/callback';
+// var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
