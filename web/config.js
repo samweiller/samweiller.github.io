a453a6295974005aeba03b7575254bd94b1591c4
Sam Weiller <sam.weiller@gmail.com>
2016-06-02 09:39:48 -0700

    added new callback URI

--- web/config.js
+++ web/config.js
@@ -1,5 +1,5 @@
 "use strict";
 
 var SPOTIFY_CLIENT_ID = '39b67cb936384107bdc3a037eec40faf';
-var SPOTIFY_REDIRECT_URI = 'http://samweiller.github.io/web/callback.html';
+var SPOTIFY_REDIRECT_URI = 'http://samweiller.github.io/web/getUserInfo.html';
 // var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
