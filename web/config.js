ad2a4792da4ffe10c58a103501499c946f7f549b
Sam Weiller <sam.weiller@gmail.com>
2016-06-06 17:36:15 -0700

    edited redirect URI

--- web/config.js
+++ web/config.js
@@ -2,4 +2,5 @@
 
 var SPOTIFY_CLIENT_ID = '39b67cb936384107bdc3a037eec40faf';
 var SPOTIFY_REDIRECT_URI = 'http://samweiller.github.io/web/getUserInfo.html';
+var SPOTIFY_REDIRECT_URI_NET = 'http://samweiller.github.io/web/networkTrainer.html';
 // var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
