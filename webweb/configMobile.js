4516ec8c31a65548879d6783348580166a6d865c
samweiller <sam.weiller@gmail.com>
2016-06-08 16:33:43 -0700

    updated config file

---
+++ webweb/configMobile.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var SPOTIFY_CLIENT_ID = '92d7ff67c7bb4d3c9bc18345d6ff7aab';
+var SPOTIFY_REDIRECT_URI = 'http://samweiller.github.io/web/getUserInfoB.html';
+// var SPOTIFY_REDIRECT_URI_NET = 'http://samweiller.github.io/web/networkTrainer.html';
+// var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
