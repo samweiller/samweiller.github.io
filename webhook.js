9b6ba6d7020f6b662dd4e751401a716c0f0a9292
samweiller <sam.weiller@gmail.com>
2016-06-16 16:32:11 -0700

    added .js

---
+++ webhook.js
@@ -0,0 +1,7 @@
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === <YOUR_VERIFY_TOKEN>) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');    
+  }
+});
