98f124e80ce35274d4e4edff13e84c525a458466
samweiller <sam.weiller@gmail.com>
2016-06-16 16:33:25 -0700

    actually added the token

--- webhook.js
+++ webhook.js
@@ -1,5 +1,5 @@
 app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === <YOUR_VERIFY_TOKEN>) {
+  if (req.query['hub.verify_token'] === 'castle_brain_table_foursquare_orientation_jupiter') {
     res.send(req.query['hub.challenge']);
   } else {
     res.send('Error, wrong validation token');    
